a0839b884e1c6529d9d12c5c055ecbc1a7e463bc
karanbht <karanbhatia507@gmail.com>
2020-10-10 16:34:30 +0530

    cors

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const port = 3001;
 
 app.use(express.json());
 
-app.all('*', function(req, res, next) {
+app.use('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
