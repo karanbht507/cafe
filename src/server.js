f8bc42ae931250906bc7743c75f03f589d7cf570
karanbht <karanbhatia507@gmail.com>
2020-10-10 16:28:43 +0530

    added cors

--- src/server.js
+++ src/server.js
@@ -7,6 +7,13 @@ const port = 3001;
 
 app.use(express.json());
 
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get('/', (req,res) => {
   res.send({ title: "Welcome to cafes" })
 })
